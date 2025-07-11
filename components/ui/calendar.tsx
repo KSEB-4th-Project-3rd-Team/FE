66a23e57465a478b8c293ada55f5c70b692fbde2
hhhwlgns <hhhwlgns@gmail.com>
2025-07-11 13:21:36 +0900

    0711/01:21

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -53,9 +53,10 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
+      
       components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
+        IconArrowLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconArrowRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
